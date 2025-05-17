39c60abe26b8f41f1aa04f72da3b99d15b2fc107
Matt Welson <matt@digitaltransformation.co.nz>
2025-05-17 16:26:26 +1200

    feat(user): :sparkles: Dummy version of a user page, only loading user info
    Not using any post data as that will be tRPC later

---
+++ src/routes/$username.tsx
@@ -0,0 +1,59 @@
+import db from "@/db";
+import { follows, posts } from "@/db/schema";
+import { createFileRoute, notFound } from "@tanstack/react-router";
+import { eq } from "drizzle-orm";
+
+export const Route = createFileRoute("/$username")({
+  component: UserPage,
+  // TODO: build a proper user not found skeleton
+  notFoundComponent: () => <div>User not found</div>,
+  loader: async ({ params }) => {
+    const user = await db.query.users.findFirst({
+      where: (users, { eq }) => eq(users.username, params.username),
+    });
+
+    if (!user) throw notFound();
+
+    const followingCount = await db.$count(
+      follows,
+      eq(follows.followingId, user.id),
+    );
+
+    const followerCount = await db.$count(
+      follows,
+      eq(follows.followerId, user.id),
+    );
+
+    const postCount = await db.$count(posts, eq(posts.userId, user.id));
+
+    return { user, followingCount, followerCount, postCount };
+  },
+});
+
+export function UserPage() {
+  const { user, followingCount, followerCount, postCount } =
+    Route.useLoaderData();
+  // TODO: load posts (and media) via tRPC
+  // TODO: find the categories users posts are in?
+  return (
+    <div>
+      <div>
+        {user.avatarUrl && (
+          <img src={user.avatarUrl} alt="" className="size-32 rounded-full" />
+        )}
+      </div>
+      <div>
+        <h1>{user.fullName}</h1>
+        <p>{user.username}</p>
+        {user.bio && <p>{user.bio}</p>}
+        {user.externalUrl && <p>{user.externalUrl}</p>}
+      </div>
+      <div>
+        <p>
+          {followerCount} followers, {followingCount} following, {postCount}{" "}
+          posts
+        </p>
+      </div>
+    </div>
+  );
+}
